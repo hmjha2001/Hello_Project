b0945cfcee21298eab531293888bf115696e63fe
Hari Mohan <hari.mohan@accolite.com>
2020-08-30 09:18:28 +0530

    Revert "for revert commit testing"
    This reverts commit 94ba7a27786055769dfe13ad46d5f26ae8a94815.

--- Test2.java
+++ Test2.java
@@ -1,10 +1,6 @@
 class Test{
-	int id;
-	Strirng name;
   public static getData(){
 	  System.out.print("get datat");
-  }
-  pulbic void calculate(){
   }
   
   public static void main(String... arg){
